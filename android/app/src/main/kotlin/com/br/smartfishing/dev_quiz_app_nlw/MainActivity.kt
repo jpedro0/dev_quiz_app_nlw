d6d68f3feffb2482e02e1ca9abdd29b914ad8aa3
JPedro Brito <jpedro0brito@gmail.com>
2021-04-19 17:26:25 -0400

    Primeira aula do nlw

---
+++ android/app/src/main/kotlin/com/br/smartfishing/dev_quiz_app_nlw/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.br.smartfishing.dev_quiz_app_nlw
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home/widgets/score_card_widget.dart
@@ -0,0 +1,45 @@
+import 'package:dev_quiz_app_nlw/core/app_colors.dart';
+import 'package:dev_quiz_app_nlw/core/app_text_styles.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'chart_widget.dart';
+
+class ScoreCardWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Container(
+        height: 136,
+        decoration: BoxDecoration(
+          color: AppColors.white,
+          borderRadius: BorderRadius.circular(15)
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 1,
+              child: ChartWidget(),
+            ),
+            Expanded(
+              flex: 3,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 24),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Vamos começa", style: AppTextStyles.heading,),
+                    Text("Completa os desafios e avance em cochecemento", style: AppTextStyles.body,),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
